1a8985ce9adcbae1b73dbb0792587a9ffa2a8bb2
Glen Sørbø <glensorbo@gmail.com>
2024-11-30 16:17:26 +0100

    feat: charts

---
+++ src/data.ts
@@ -0,0 +1,83 @@
+type Player = 'Glen' | 'Thomas' | 'Margaux' | 'Marta' | 'Thorjan' | 'Tor Arve';
+
+export const data: Record<'date' | Player | 'Winner', string | number>[] = [
+  {
+    date: '2024-11-29',
+    Glen: 12320,
+    Thomas: 7825,
+    Margaux: 9324,
+    Thorjan: 11426,
+    'Tor Arve': 0,
+    Marta: 0,
+    Winner: 'Glen',
+  },
+  {
+    date: '2024-11-22',
+    Glen: 0,
+    Thomas: 8315,
+    Margaux: 13558,
+    Thorjan: 11363,
+    'Tor Arve': 11847,
+    Marta: 4707,
+    Winner: 'Margaux',
+  },
+];
+
+export const getPlayerDetails = () => {
+  const details: {
+    name: string;
+    played: number;
+    won: number;
+    points: { name: string; date: string; total: number }[];
+  }[] = [
+    { name: 'Glen', won: 0, played: 0, points: [] },
+    { name: 'Thomas', won: 0, played: 0, points: [] },
+    { name: 'Thorjan', won: 0, played: 0, points: [] },
+    { name: 'Margaux', won: 0, played: 0, points: [] },
+    { name: 'Tor Arve', won: 0, played: 0, points: [] },
+    { name: 'Marta', won: 0, played: 0, points: [] },
+  ];
+  for (const d of data.toSorted(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+  )) {
+    for (const [key, value] of Object.entries(d)) {
+      const player = details.find((d) => d.name === key);
+      if (!player) {
+        continue;
+      }
+
+      if (player.name === d.Winner) {
+        player.won += 1;
+      }
+
+      if (typeof value === 'number' && value > 0) {
+        player.played = +1;
+      }
+
+      const currentPoints = player.points.at(-1)?.total ?? 0;
+
+      if (typeof value === 'number' && typeof d.date === 'string') {
+        player.points.push({ name: player.name, date: d.date, total: currentPoints + value });
+      }
+    }
+  }
+
+  const points: Record<string, string | number>[] = [];
+
+  for (const player of details) {
+    for (const score of player.points) {
+      const date = points.find((p) => p.date === score.date);
+
+      if (date) {
+        date[player.name] = score.total;
+      } else {
+        points.push({
+          date: score.date,
+          [player.name]: score.total,
+        });
+      }
+    }
+  }
+
+  return { details, points };
+};
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,30 +1,12 @@
-import dayjs from 'dayjs';
-import { LineChart } from '@mantine/charts';
-import { Flex } from '@mantine/core';
+import { BarChart, LineChart } from '@mantine/charts';
+import { Flex, Title } from '@mantine/core';
+import { data, getPlayerDetails } from '@/data';
 
 export const HomePage = () => {
-  const data: Record<string, any>[] = [
-    {
-      date: '2024-11-22',
-      Glen: 0,
-      Thomas: 8315,
-      Margaux: 13558,
-      Thorjan: 11363,
-      'Tor Arve': 11847,
-      Marta: 4707,
-    },
-    {
-      date: '2024-11-29',
-      Glen: 12320,
-      Thomas: 7825,
-      Margaux: 9324,
-      Thorjan: 11426,
-      'Tor Arve': 0,
-      Marta: 0,
-    },
-  ];
+  const { details, points } = getPlayerDetails();
   return (
-    <Flex align="center" justify="center">
+    <Flex align="center" justify="center" direction="column" gap={50} pt={50}>
+      <Title>Wise GeoGuesser results</Title>
       <LineChart
         h={300}
         w="75%"
@@ -42,7 +24,40 @@ export const HomePage = () => {
           { name: 'Margaux', color: 'indigo.6' },
           { name: 'Tor Arve', color: 'orange.6' },
           { name: 'Marta', color: 'violet.6' },
+        ].toSorted((a, b) => a.name.localeCompare(b.name))}
+      />
+      <Title>Won vs played</Title>
+      <BarChart
+        h={300}
+        w="75%"
+        data={details}
+        withLegend
+        legendProps={{ verticalAlign: 'bottom', height: 50 }}
+        dataKey="name"
+        series={[
+          { name: 'played', color: 'violet.6', label: 'Played' },
+          { name: 'won', color: 'green.8', label: 'Won' },
         ]}
+      />
+      <Title>Accumulated points over time</Title>
+      <LineChart
+        h={300}
+        w="75%"
+        data={points}
+        dataKey="date"
+        curveType="natural"
+        tickLine="x"
+        gridAxis="xy"
+        withLegend
+        legendProps={{ verticalAlign: 'bottom', height: 50 }}
+        series={[
+          { name: 'Glen', color: 'pink.6' },
+          { name: 'Thorjan', color: 'red.6' },
+          { name: 'Thomas', color: 'lime.6' },
+          { name: 'Margaux', color: 'indigo.6' },
+          { name: 'Tor Arve', color: 'orange.6' },
+          { name: 'Marta', color: 'violet.6' },
+        ].toSorted((a, b) => a.name.localeCompare(b.name))}
       />
     </Flex>
   );
