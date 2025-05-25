530e963ff69ea179ba83c101024672aeac20fa8c
Glen Sørbø <glensorbo@gmail.com>
2025-05-25 22:03:12 +0200

    registration

--- backend/Contracts/Authentication/AuthenticationRequest.cs
+++ backend/Contracts/Authentication/AuthenticationRequest.cs
@@ -1,5 +1,14 @@
 namespace Contracts.Authentication
 {
-  public record AuthenticationRequest(string Username, string Password);
+  public class AuthenticationRequest
+  {
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+
+    public bool IsValid()
+    {
+      return !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
+    }
+  }
 }
 
